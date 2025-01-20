dacf0879b0e500803175340536645791f12560b3
Pranay <155623993+PranayBollempelli@users.noreply.github.com>
2025-01-20 12:41:06 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function adjustPageSize() {
+    const screenWidth = window.innerWidth;
+    const body = document.body;
+
+    if (screenWidth >= 992 && screenWidth <= 1600) {
+        body.style.transform = 'scale(0.9)';
+    } else if (screenWidth >= 700 && screenWidth <= 767) {
+        body.style.transform = 'scale(0.8)';
+    } else if (screenWidth >= 600 && screenWidth <= 700) {
+        body.style.transform = 'scale(0.75)';
+    } else if (screenWidth <= 600) {
+        body.style.transform = 'scale(0.5)';
+    } else {
+        body.style.transform = 'scale(1)';
+    }
+}
+
+window.addEventListener('resize', adjustPageSize);
+window.addEventListener('load', adjustPageSize);
